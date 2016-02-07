bbbf019887ce53ec55e71194240fc97257f32a77
toashel <shelchean@gmail.com>
2016-02-07 17:20:33 +1300

    Finish layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,23 @@
 Rails.application.routes.draw do
-  get 'static_pages/about'
-  get 'static_pages/help'
+  get 'users/new'
+
+  get 'help'    => 'static_pages#help'
+  get 'about'   => 'static_pages#about'
+  get 'contact' => 'static_pages#contact'
+  get 'signup'  => 'users#new'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'static_pages#home'
+  root :to => 'static_pages#home'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
-
+  resources :posts
   # Example resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
 
